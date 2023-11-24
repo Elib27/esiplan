317546f49a076ada7c58b77a061e6616b9faa62a
Eliot Bas <eliot.bas@orange.fr>
2023-11-24 14:45:37 +0100

    add keys date navigation

--- src/components/dateNavigation.tsx
+++ src/components/dateNavigation.tsx
@@ -1,4 +1,4 @@
-import { Dispatch, SetStateAction } from 'react'
+import { Dispatch, SetStateAction, useEffect } from 'react'
 import addDaysToDate from '@/lib/addDaysToDate'
 import ArrowLeftIcon from '@/assets/arrow-left.svg'
 import ArrowRightIcon from '@/assets/arrow-right.svg'
@@ -9,29 +9,50 @@ export default function DateNavigation({
 }: {
   setScheduleDate: Dispatch<SetStateAction<Date>>
 }) {
-  const handleClickPrevDay = () => setScheduleDate((d) => addDaysToDate(d, -1))
-  const handleClickNextDay = () => setScheduleDate((d) => addDaysToDate(d, 1))
-  const handleClickToday = () => setScheduleDate(new Date())
+  useEffect(() => {
+    function handleKeyDown(e: KeyboardEvent) {
+      switch (e.key) {
+        case 'ArrowLeft':
+          setScheduleDate((d) => addDaysToDate(d, -1))
+          break
+        case 'ArrowRight':
+          setScheduleDate((d) => addDaysToDate(d, 1))
+          break
+        case 'ArrowUp':
+          setScheduleDate((d) => addDaysToDate(d, 7))
+          break
+        case 'ArrowDown':
+          setScheduleDate((d) => addDaysToDate(d, -7))
+          break
+        case ' ':
+          setScheduleDate(new Date())
+      }
+    }
+
+    window.addEventListener('keydown', handleKeyDown)
+
+    return () => window.removeEventListener('keydown', handleKeyDown)
+  }, [setScheduleDate])
 
   return (
     <div className='flex items-center gap-10 p-4'>
       <button
         className='flex aspect-square h-14 items-center justify-center rounded-full bg-light-purple active:scale-95'
-        onClick={handleClickPrevDay}
+        onClick={() => setScheduleDate((d) => addDaysToDate(d, -1))}
         aria-label='Jour précédent'
       >
         <ArrowLeftIcon className='relative right-px' />
       </button>
       <button
         className='flex aspect-square h-14 items-center justify-center rounded-full bg-light-purple active:scale-95'
-        onClick={handleClickToday}
+        onClick={() => setScheduleDate(new Date())}
         aria-label="Aujourd'hui"
       >
         <TodayIcon />
       </button>
       <button
         className='flex aspect-square h-14 items-center justify-center rounded-full bg-light-purple active:scale-95'
-        onClick={handleClickNextDay}
+        onClick={() => setScheduleDate((d) => addDaysToDate(d, 1))}
         aria-label='Jour suivant'
       >
         <ArrowRightIcon className='relative left-px' />
